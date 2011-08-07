52cd2381e53d5f22a4471e7c3b442906509f84ae
Shigeaki Matsumura <matsu911@gmail.com>
2011-08-08 08:42:39 +0900

    first commit

---
+++ spim-mode.el
@@ -0,0 +1,32 @@
+(require 'asm-mode)
+(require 'comint)
+
+(defvar *lua-process-buffer* nil
+  "Buffer used for communication with SPIM subprocess")
+
+(defun spim-mode ()
+  (interactive)
+  (asm-mode)
+  (setq mode-name "Assembler+SPIM")
+  (define-key asm-mode-map (kbd "C-c C-l") 'spim-exec-file)
+  (use-local-map (nconc (make-sparse-keymap) asm-mode-map)))
+
+(defun spim-exec-file ()
+  (interactive)
+  (when (buffer-name *lua-process-buffer*)
+    (kill-buffer *lua-process-buffer*))
+  (progn
+    (setq *lua-process-buffer* (apply 'make-comint "spim" "spim" nil '())) 
+    (delete-other-windows)
+    (switch-to-buffer-other-window *lua-process-buffer*)
+    (other-window -1))
+  (let ((filename (buffer-file-name (current-buffer))))
+    (flet ((send-command (command)
+			 (comint-send-string (get-buffer-process *lua-process-buffer*) command)))
+      (send-command (format "load \"%s\"\n" (buffer-file-name (current-buffer)))) 
+      (send-command "run\n"))))
+
+;;;###autoload
+(add-to-list 'auto-mode-alist '("\\.as$" . spim-mode))
+
+(provide 'spim-mode)
