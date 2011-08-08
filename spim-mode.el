170ec41f932d4c36f7d8741b420c3478c66adbc6
Shigeaki Matsumura <matsu911@gmail.com>
2011-08-08 23:20:48 +0900

    added spim-load-file, spim-reinitialize, spim-run and spim-display-buffer

--- spim-mode.el
+++ spim-mode.el
@@ -8,23 +8,46 @@
   (interactive)
   (asm-mode)
   (setq mode-name "Assembler+SPIM")
-  (define-key asm-mode-map (kbd "C-c C-l") 'spim-exec-file)
+  (define-key asm-mode-map (kbd "C-c C-l") 'spim-load-file)
+  (define-key asm-mode-map (kbd "C-c C-i") 'spim-reinitialize)
+  (define-key asm-mode-map (kbd "C-c C-r") 'spim-run)
+  (define-key asm-mode-map (kbd "C-c C-z") 'spim-display-buffer)
   (use-local-map (nconc (make-sparse-keymap) asm-mode-map)))
 
-(defun spim-exec-file ()
+(defun spim-send-command (command)
+  (comint-send-string (get-buffer-process *spim-process-buffer*) command))
+
+(defun spim-reinitialize ()
   (interactive)
-  (flet ((send-command (command)
-		       (comint-send-string (get-buffer-process *spim-process-buffer*) command)))
-    (when *spim-process-buffer*
-      (process-kill-without-query (get-buffer-process *spim-process-buffer*))
-      (kill-buffer *spim-process-buffer*))
-    (progn
-      (setq *spim-process-buffer* (apply 'make-comint "spim" "spim" nil '())) 
-      (delete-other-windows)
-      (switch-to-buffer-other-window *spim-process-buffer*)
-      (other-window -1))
-    (send-command (format "load \"%s\"\n" (buffer-file-name (current-buffer)))) 
-    (send-command "run\n")))
+  (unless *spim-process-buffer*
+    (spim-create-buffer))
+  (spim-send-command "reinitialize\n"))
+
+(defun spim-create-buffer ()
+  (setq *spim-process-buffer* (apply 'make-comint "spim" "spim" nil '())) 
+  (delete-other-windows)
+  (switch-to-buffer-other-window *spim-process-buffer*)
+  (other-window -1)) 
+
+(defun spim-load-file ()
+  (interactive)
+  (unless (and *spim-process-buffer*
+	       (buffer-name *spim-process-buffer*))
+    (spim-create-buffer))
+  (spim-send-command (format "load \"%s\"\n" (buffer-file-name (current-buffer)))) )
+
+(defun spim-run ()
+  (interactive)
+  (unless (and *spim-process-buffer*
+	       (buffer-name *spim-process-buffer*))
+    (spim-create-buffer))
+  (spim-send-command "run\n"))
+
+(defun spim-display-buffer ()
+  (interactive)
+  (when (and *spim-process-buffer*
+	     (buffer-name *spim-process-buffer*))
+    (display-buffer *spim-process-buffer*)))
 
 ;;;###autoload
 (add-to-list 'auto-mode-alist '("\\.as$" . spim-mode))
